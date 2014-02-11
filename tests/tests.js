a7f24f612c181224750baf079e36780ba3a4af8f
Gonzalo J. García Martín <=>
2014-02-11 19:34:11 +0000

    Ahora parece que el test no existe

--- tests/tests.js
+++ tests/tests.js
@@ -1,10 +1,11 @@
 var assert = chai.assert;
+var tabla1 = "<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>3</td>                                  <td> 2</td>                                  <td> F</td>              </tr>\n</tbody></table>";
 
 suite('temperature', function() {
     test('3, 2, F = [3, 2, F]', function() {
         original.value = "3, 2, F";
         calculate();
-        assert.deepEqual(finaltable.innerHTML, "[3, 2, F]");
+        assert.deepEqual(finaltable.innerHTML, tabla1);
     });
 //     test('45C = 113.0 Farenheit', function() {
 //         original.value = "45C";
