bdb54d8eeb1b2871002d4838849ca54704616ad5
Gonzalo Garcia <alu0100403619@ull.edu.es>
2014-02-13 11:11:06 +0000

    Test mocha y chai implementados correctamente

--- tests/tests.js
+++ tests/tests.js
@@ -1,5 +1,6 @@
 var assert = chai.assert;
-var tabla1 = "<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>3</td>                                  <td> 2</td>                                  <td> F</td>              </tr>\n</tbody></table>";
+var tabla1 = '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>3</td>                                  <td> 2</td>                                  <td> F</td>              </tr>\n</tbody></table>';
+var tabla2 = '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>4</td>                                  <td> 5</td>                                  <td> C 6</td>                                  <td> 7</td>                                  <td> D</td>              </tr>\n</tbody></table>';
 
 suite('temperature', function() {
     test('3, 2, F = [3, 2, F]', function() {
@@ -7,14 +8,14 @@ suite('temperature', function() {
         calculate();
         assert.deepEqual(finaltable.innerHTML, tabla1);
     });
-//     test('45C = 113.0 Farenheit', function() {
-//         original.value = "45C";
-//         calculate();
-//         assert.deepEqual(finaltable.innerHTML, "113 Grados Farenheit");
-//     });
-//     test('5X = error', function() {
-//         original.value = "5X";
-//         calculate();
-//         assert.match(finaltable.innerHTML, /ERROR/);
-//     });
+     test('4, 5, C \n 6, 7, D = [[4, 5, C], [6, 7, D]]', function() {
+         original.value = "4, 5, C\n 6, 7, D";
+         calculate();
+         assert.deepEqual(finaltable.innerHTML, tabla2);
+     });
+     test('5, X \n 6, Y, Z es una tabla', function() {
+         original.value = "5, X\n 6, Y, Z";
+         calculate();
+         assert.match(finaltable.innerHTML, /table/);
+     });
 });
